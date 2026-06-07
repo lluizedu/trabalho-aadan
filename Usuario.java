550c040b68c6084068832372e6e7dd93dc546498
lluizedu <Cavalodoido283@gmail.com>
2026-06-07 13:39:51 -0300

    Create Usuario.java

---
+++ Usuario.java
@@ -0,0 +1,25 @@
+package br.ucb.biblioteca.model;
+
+public class Usuario {
+    private int id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private boolean ativo;
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getTelefone() { return telefone; }
+    public void setTelefone(String telefone) { this.telefone = telefone; }
+    public boolean isAtivo() { return ativo; }
+    public void setAtivo(boolean ativo) { this.ativo = ativo; }
+
+    @Override
+    public String toString() {
+        return id + " - " + nome;
+    }
+}
